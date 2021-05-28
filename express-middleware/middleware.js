8edb568b193bb08cf721284a12cc80cfb1bcfd09
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2021-05-28 18:12:55 +0530

    Reformatted folders

---
+++ express-middleware/middleware.js
@@ -0,0 +1,47 @@
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+
+var PROTO_PATH = './logger.proto';
+
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+
+var logger = grpc.loadPackageDefinition(packageDefinition).logger;
+
+var target = 'localhost:9000' // Replace with server URL
+
+
+const loggerMiddleware = (req, _, next) => {
+    try {
+        const { method, url, httpVersion, headers } = req;
+        const ip_address = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        const log = {
+            timeOfLog : new Date().toISOString(),
+            method,
+            url,
+            httpVersion,
+            headers,
+            remoteAddress : ip_address
+        }
+        var client = new logger.LogService(target,
+            grpc.credentials.createInsecure());
+
+        client.LogRequest({ body: JSON.stringify(log) }, function (err, _) {
+            if (err) throw err;
+        });
+    } catch (err) {
+        console.log(err);
+    } finally {
+        next();
+    }
+}
+
+
+module.exports = loggerMiddleware;
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/Ash110/grpc-logger
 go 1.16
 
 require (
-	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 
 	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
 	google.golang.org/genproto v0.0.0-20210524171403-669157292da3 // indirect
 	google.golang.org/grpc v1.38.0
